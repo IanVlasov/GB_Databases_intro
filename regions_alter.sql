f5a93648c92a5a561af2d24774533c81a0aadcce
IanVlasov <43469536+IanVlasov@users.noreply.github.com>
2020-11-01 20:33:29 +0300

    HW02

---
+++ regions_alter.sql
@@ -0,0 +1,26 @@
+ALTER TABLE `geodata`.`_regions` 
+DROP COLUMN `title_cz`,
+DROP COLUMN `title_lv`,
+DROP COLUMN `title_lt`,
+DROP COLUMN `title_ja`,
+DROP COLUMN `title_pl`,
+DROP COLUMN `title_it`,
+DROP COLUMN `title_fr`,
+DROP COLUMN `title_de`,
+DROP COLUMN `title_pt`,
+DROP COLUMN `title_es`,
+DROP COLUMN `title_en`,
+DROP COLUMN `title_be`,
+DROP COLUMN `title_ru`,
+CHANGE COLUMN `region_id` `id` INT NOT NULL AUTO_INCREMENT ,
+CHANGE COLUMN `title_ua` `title` VARCHAR(150) NOT NULL ,
+ADD PRIMARY KEY (`id`),
+ADD INDEX `title` (`title` ASC) VISIBLE,
+ADD INDEX `fk_regions_countries_id_idx` (`country_id` ASC) VISIBLE;
+;
+ALTER TABLE `geodata`.`_regions` 
+ADD CONSTRAINT `fk_regions_countries_id`
+  FOREIGN KEY (`country_id`)
+  REFERENCES `geodata`.`_countries` (`id`)
+  ON DELETE RESTRICT
+  ON UPDATE RESTRICT;
